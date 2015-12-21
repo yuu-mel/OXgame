ebda05662bc0d3b17a7f51842359f22293245ab8
unk931 <yamaguchi_1610@yahoo.co.jp>
2015-12-22 01:23:19 +0900

    new

--- src/unk/Unk.java
+++ src/unk/Unk.java
@@ -16,7 +16,7 @@ public class Unk {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("");
+        System.out.println("‚Ä‚·‚Ä‚·");
     }
     
 }
