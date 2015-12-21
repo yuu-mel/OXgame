20e6425239fe8c8f44c723231bd22bf5912062a4
unk931 <yamaguchi_1610@yahoo.co.jp>
2015-12-22 00:18:11 +0900

    test

---
+++ Test/src/test/Main.java
@@ -0,0 +1,13 @@
+package oxgame;
+
+public class Main {
+
+	public static void main(String[] args)
+	{
+System.out.println("ã?ã‚“ã“");
+//â†‘ã“ã?ã¤æœ?é«˜ã«ã‚¢ãƒ?
+//ã?ã‚“ã“ã£ã?
+//‘‚«Š·‚¦ƒeƒXƒg
+	}
+
+}
